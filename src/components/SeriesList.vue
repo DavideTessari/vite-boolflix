99216734f5e12fe8a14b3ca7d2204a2688a911c9
DavideTessari <davtex@gmail.com>
2024-04-17 15:52:45 +0200

    first commit

---
+++ src/components/SeriesList.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from '../store';
+import CountryFlag from "vue-country-flag-next";
+
+// import LangFlag from 'vue-lang-code-flags';
+
+
+export default {
+data(){
+    return{
+        store,
+    
+
+    }
+},
+
+components:{
+    CountryFlag,
+    // LangFlag,
+},
+
+
+}
+</script>
+
+
+
+<template>
+ 
+  
+
+</template>
+
+
+
+<style lang="scss"  scoped>
+@use '../style/partials/variables.scss'as *;
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {reactive} from 'vue';
+
+export const store = reactive(
+    
+    {
+        ApiKey: "dd6f748407d41158f1d47125a3b3fd33",
+        FilmList: [],
+        SeriesList: [],
+        Searchtext: "",
+    }
+);
