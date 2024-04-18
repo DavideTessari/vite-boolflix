455c722b38013130d2f10167c66086c6e6dad4d5
DavideTessari <davtex@gmail.com>
2024-04-18 14:44:18 +0200

    feat: add card and complete layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
-<script >
+<script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import axios from 'axios';
-import { store } from './store';
 import AppCardFilm from './components/AppCardFilm.vue';
 import AppSearchbar from './components/AppSearchbar.vue';
+import { store } from './store';
+import axios from 'axios';
 
-export default{
+export default {
   name: 'App',
-  components:{
+  components: {
     AppHeader,
     AppMain,
     AppCardFilm,
-    AppCardSerie,
-    AppSearchbar,
+    AppSearchbar
   },
-  data(){
-    return{
-      store,
-    }
+  data() {
+    return {
+      store
+    };
   }
-}
+};
 </script>
 
 <template>
   <div>
-    <AppHeader/>
-    <AppMain/>
+    <AppHeader />
+    <AppMain />
   </div>
 </template>
 
